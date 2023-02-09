1e16446e768c55c925eb088e8f2259bde8eeb0f5
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2023-02-09 15:55:50 +0100

    button

--- src/components/TailwindButton.js
+++ src/components/TailwindButton.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const TailwindButton = (props) => {
   return (
-    <div className='bg-blue-500 text-white font-medium px-4 py-2 rounded hover:bg-blue-800'>TailwindButton</div>
+    <div className='bg-blue-500 text-white font-medium px-4 py-2 rounded hover:bg-blue-800 shadow-lg border border-slate-600 absolute bottom-4 right-4'>{props.children}</div>
   )
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,10 @@
+import { TailwindButton } from "../components/TailwindButton";
 const Search = () => {
   return (<>
   <div className="w-1 h-1"></div>
   <div>
     <h1 className="h-screen bg-blue-600 text-9xl ">Search</h1>
-    <div className="w-80 h-60 absolute right-1/2 top-1/2 border border-solid border-black bg-slate-500"> hello</div>
+    <div className="w-80 h-60 absolute right-1/2 top-1/2 border border-solid border-black bg-slate-500"> hello <TailwindButton>Button</TailwindButton></div>
 
   </div>
   </>)
