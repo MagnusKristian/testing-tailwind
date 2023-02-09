8b6fd30466bbbb44ad74e537d8b19f102aa8cc7e
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2023-02-09 14:57:20 +0100

    button

---
+++ src/components/TailwindButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TailwindButton = (props) => {
+  return (
+    <div className='bg-blue-500 text-white font-medium px-4 py-2 rounded hover:bg-blue-800'>TailwindButton</div>
+  )
+}
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,6 +3,7 @@ const Search = () => {
   <div className="w-1 h-1"></div>
   <div>
     <h1 className="h-screen bg-blue-600 text-9xl ">Search</h1>
+    <div className="w-80 h-60 absolute right-1/2 top-1/2 border border-solid border-black bg-slate-500"> hello</div>
 
   </div>
   </>)
